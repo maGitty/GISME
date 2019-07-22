a5f8bb364ac26b3bf1867f5fe4a6aa29639d6a2d
Unknown <mahik18@gmx.de>
2019-07-22 18:26:01 +0200

    code: - implemented filtering for wether points are within germany or not - plotting now custom grid to exactly fit the data points - using different shape file for plots as the old one was different from the one used for filtering - stretched load plots more - added first prediction models using arma methods, not working correctly (concerning parameter choice) - added methods to do acf plotting switched to use nan functions to avoid errors when for nan data which occurs e.g. due to filtering
    thesis:
    - split load plot in 2 figs for better readability
    - added figures to visualize process of filtering data within germany
    - started explaining first methods
    - kind of finished rw part, added papers/descriptions, adjusted table
    - added eurostat/nuts references
    - temporarily removed title background, maybe going to add something more simple later
    - switched to tabularx due to easier handling of linebreaks within cells

--- code/DataPlotter.py
+++ code/DataPlotter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from glob_vars import (figure_path,lon_col,lat_col,de_load,hertz_load,amprion_load,
-                       tennet_load,transnet_load,bbox,variable_dictionary,data_path)
+                       tennet_load,transnet_load,bbox,variable_dictionary,data_path,nuts3_01res_shape,nuts0_shape)
 from WeatherReader import WeatherReader
 from LoadReader import LoadReader
 
@@ -11,7 +11,8 @@ import pandas as pd
 import numpy as np
 import shapefile as shp
 from datetime import datetime, time
-from matplotlib import pyplot as plt, colors, ticker, cm
+from descartes import PolygonPatch
+from matplotlib import pyplot as plt, colors, ticker, cm, markers
 #import seaborn as sns
 #import xarray as xr
 #from calendar import monthrange
@@ -25,7 +26,7 @@ class DataPlotter:
     """
     handles plotting for load and weather data
     """
-    def __init__(self,fmt='pdf',save=True,show=False,shape=None):
+    def __init__(self,fmt='pdf',save=True,show=False,shape=None,isin=False):
         """Initializes WeatherPlot instance
         
         Parameters
@@ -36,6 +37,10 @@ class DataPlotter:
                 wether to save plots to file or not
         show  : boolean
                 wether to show plots or not
+        shape : tuple
+                if multiplot should be plotted, specify shape
+        isin  : boolean
+                wether to filter values from outside germany
         
         Returns
         -------
@@ -47,24 +52,53 @@ class DataPlotter:
         self.save = save
         self.show = show
         self.shape = shape
+        self.isin = isin
+    
+    def __save_show_fig(self,fig,dir_pth,file_name):
+        """TODO
+        
+        """
+        if self.save:
+            print(f'saving plot in {file_name}')
+            if not os.path.exists(dir_pth):
+                os.makedirs(dir_pth)
+            if type(self.fmt) is list:
+                for f in self.fmt:
+                    fig.savefig(f'{file_name}.{f}', bbox_inches='tight', format=f, optimize=True, dpi=150)
+            else:
+                fig.savefig(f'{file_name}.{self.fmt}', bbox_inches='tight', format=self.fmt, optimize=True, dpi=150)
+        if self.show:
+            plt.show()
+        # close figures as they won't be closed automatically by python during runtime
+        plt.close(fig)
     
     def __create_fig_map(self,ax,variable,day,norm,xlbl_true=None,ylbl_true=None):
         """TODO
         
         """
-        data = self.wreader.vals4time(variable, day)
+        if self.isin:
+            data = self.wreader.vals4time_isin(variable,day)
+            #img = ax.imshow(data.values, cmap='jet', extent=bbox, norm=norm)
+            img = data.plot.imshow(ax=ax,cmap='jet', extent=bbox, norm=norm, add_colorbar=False)
+        else:
+            data = self.wreader.vals4time(variable, day)
+            img = ax.imshow(data.values, cmap='jet', extent=bbox, interpolation='bilinear', norm=norm)
+            #img = data.plot.imshow(ax=ax,cmap='jet', extent=bbox, interpolation='bilinear', norm=norm, add_colorbar=False)
         
-        img = ax.imshow(data.values, cmap='jet', extent=bbox, interpolation='bilinear', norm=norm)
+        #read shapefile
+        eu_shape = shp.Reader(nuts0_shape)
+        for record in eu_shape.shapeRecords():
+            if 'DE' in record.record:
+                de_shape = record
+                break
         
-        # read shapefile
-        sf = shp.Reader('/home/marcel/Dropbox/data/shapes/DEU_adm1.shp')
-        # iterate over all 16 states, in DEU_adm0.shp there is only one shape for germany
-        for state in sf.shapes():
-            points = np.array(state.points)
-            intervals = list(state.parts) + [len(state.points)]
-            ax.set_aspect(1)
-            for (x, y) in zip(intervals[:-1], intervals[1:]):
-                ax.plot(*zip(*points[x:y]), color='k', linewidth=.4)
+        # concatenate points so that single lines can be drawn
+        state = de_shape.shape
+        points = np.array(state.points)
+        intervals = list(state.parts) + [len(state.points)]
+        for (x, y) in zip(intervals[:-1], intervals[1:]):
+            plt.plot(*zip(*points[x:y]), color='k', linewidth=2)
+                
         ax.set_title(pd.to_datetime(day).strftime("%Y/%m/%d %HH"))
         # print x and y label only if is most left/lowest plot
         if xlbl_true:
@@ -78,8 +112,17 @@ class DataPlotter:
         yticks = np.linspace(bbox[2],bbox[3],(bbox[3]-bbox[2])*4+1)
         ax.set_yticks(yticks,minor=True)
         
-        ax.grid(which='minor',alpha=0.2,color='k',linewidth=.5,linestyle='--')
-        ax.grid(which='major',alpha=0.4,color='k',linewidth=.5,linestyle='--')
+        # plot own grid
+        xgrid = np.linspace(bbox[0]+.125, bbox[1]-.125, num=(bbox[1]-bbox[0])*4)
+        ygrid = np.linspace(bbox[2]+.125, bbox[3]-.125, num=(bbox[3]-bbox[2])*4)
+        for xpoint in xgrid:
+            ax.axvline(xpoint,alpha=.2,color='k',linewidth=.5,linestyle='--')
+        for ypoint in ygrid:
+            ax.axhline(ypoint,alpha=.2,color='k',linewidth=.5,linestyle='--')
+        #ax.grid(b=True,xdata=xgrid,ydata=ygrid,alpha=.2,color='k',linewidth=.5,linestyle='--')
+        
+        #ax.grid(which='minor',alpha=0.2,color='k',linewidth=.5,linestyle='--')
+        #ax.grid(which='major',alpha=0.4,color='k',linewidth=.5,linestyle='--')
     
     def __plot_days(self, days, fname):
         """Plot data for each day in days list and save file with specified format
@@ -124,22 +167,10 @@ class DataPlotter:
             cbar = fig.colorbar(smap, ticks=cbox_ticks, ax=axs.ravel().tolist())
             cbar.set_label(self.wreader.get_long_name(var))
             
-            if self.save:
-                dir_pth = f'{figure_path}{var}/bundles/'
-                file_name = f'{dir_pth}{fname}{len(days)}_maps'
-                print(f'saving plot for {day} in {file_name}')
-                if not os.path.exists(dir_pth):
-                    os.makedirs(dir_pth)            
-                
-                if type(self.fmt) is list:
-                    for f in self.fmt:
-                        fig.savefig(f'{file_name}.{f}', bbox_inches='tight', format=f, optimize=True, dpi=150)                    
-                else:
-                    fig.savefig(f'{file_name}.{self.fmt}', bbox_inches='tight', format=self.fmt, optimize=True, dpi=150)
-            if self.show:
-                plt.show()
-            # close figures as they won't be closed automatically by python during runtime
-            plt.close(fig)            
+            dir_pth = f'{figure_path}{var}/bundles/'
+            file_name = f'{dir_pth}{fname}{len(days)}_maps{"_isin" if self.isin else ""}'
+            
+            self.__save_show_fig(fig, dir_pth, file_name)
         else:
             for day_num, day in enumerate(days["time"].values):
                 fig, ax = plt.subplots()
@@ -149,21 +180,10 @@ class DataPlotter:
                 cbar = fig.colorbar(smap,ticks=cbox_ticks)
                 cbar.set_label(self.wreader.get_long_name(var))
         
-                if self.save:
-                    dir_pth = f'{figure_path}{var}/{fname}/'
-                    file_name = f'{dir_pth}{day_num}_map'
-                    print(f'saving plot for {day} in {file_name}')
-                    if not os.path.exists(dir_pth):
-                        os.makedirs(dir_pth)
-                    if type(self.fmt) is list:
-                        for f in self.fmt:
-                            fig.savefig(f'{file_name}.{f}', bbox_inches='tight', format=f, optimize=True, dpi=150)
-                    else:
-                        fig.savefig(f'{file_name}.{self.fmt}', bbox_inches='tight', format=self.fmt, optimize=True, dpi=150)
-                if self.show:
-                    plt.show()
-                # close figures as they won't be closed automatically by python during runtime
-                plt.close(fig)
+                dir_pth = f'{figure_path}{var}/{fname}/'
+                file_name = f'{dir_pth}{day_num}_map{"_isin" if self.isin else ""}'
+                
+                self.__save_show_fig(fig, dir_pth, file_name)
     
     def plot_nmin(self, var, n=4):
         """Plot/save the n days with the smallest values for the specified
@@ -372,21 +392,16 @@ class DataPlotter:
         try:
             contained = np.load(f'{data_path}isin.npy')
         except:
+            print(f'isin file not found in {data_path}')
             contained = self.wreader.check_isinDE()
         
         fig,ax = plt.subplots()
         ax.imshow(contained,cmap=plt.cm.gray, extent=bbox)
         
-        if self.save:
-            file_name = figure_path + 'isin'
-            print(f'saving plot in {file_name}')
-            fig.savefig(f'{file_name}.{self.fmt}', bbox_inches='tight', format=self.fmt, optimize=True, dpi=150)
-        if self.show:
-            plt.show()
-        # close figures as they won't be closed automatically by python during runtime
-        plt.close(fig)        
+        file_name = figure_path + 'isinDE'
+        self.__save_show_fig(fig, figure_path, file_name)
     
-    def plot_load_time_func(self, var, start, stop, func, load_col=de_load, freq=24):
+    def plot_load_time_func(self, var, start, stop, func, load_col=de_load, freq=24,aspect=(12,5),skip_bottom_labels=False):
         """Plot/save function of load and date with variable
            after applying given function to its data
         
@@ -428,11 +443,11 @@ class DataPlotter:
         rngwend = rng.where((rng.weekday == 5) | (rng.weekday == 6), other=pd.NaT).dropna()
         
         # use figsize to stretch
-        fig, ax = plt.subplots(figsize=(12,5))
-        ax.scatter(rngwend, loadwend, s=8, c=ncwend, cmap='jet', edgecolors='none', label='weekend', marker="^", facecolors='none')
-        ax.scatter(rngweek, loadweek, s=6, c=ncweek, cmap='jet', edgecolors='none', label='workday')
+        fig, ax = plt.subplots(figsize=aspect)
+        ax.scatter(rngwend, loadwend, s=24, c=ncwend, cmap='jet', edgecolors='none', label='weekend', marker=markers.MarkerStyle(marker="^", fillstyle='none'))
+        ax.scatter(rngweek, loadweek, s=12 , c=ncweek, cmap='jet', edgecolors='none', label='workday')
         ax.set_ylabel(variable_dictionary[load_col])
-        ax.legend(markerscale=2)
+        ax.legend()#markerscale=2)
         
         cmap = plt.get_cmap('jet',256)
         norm = colors.Normalize(vmin=ncval.min(),vmax=ncval.max())
@@ -442,22 +457,18 @@ class DataPlotter:
         cbar = fig.colorbar(scal_map,ticks=ticks,ax=ax)#,pad=.04,shrink=.6) # shrink to fit if aspect is changed
         cbar.ax.set_ylabel(f'{self.wreader.get_long_name(var)} {fname} reduce over DE (K)', rotation=90, rotation_mode='anchor')
         
-        # rotate labels from x-axis by 30° for readability
-        plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
+        if not skip_bottom_labels:
+            # rotate labels from x-axis by 30° for readability
+            plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
+        else:
+            ax.set_xticklabels([])
         
-        if self.save:
-            pth = figure_path + 'plot_load_time_func/'
-            file_name = pth + f'{var}_{fname}_{start.strftime("%Y%m%d%H")}_{stop.strftime("%Y%m%d%H")}_{freq}F'
-            print(f'saving plot in {file_name}')
-            if not os.path.exists(pth):
-                os.makedirs(pth)
-            fig.savefig(f'{file_name}.{self.fmt}', bbox_inches='tight', format=self.fmt, optimize=True, dpi=150)
-        if self.show:
-            plt.show()
-        # close figures as they won't be closed automatically by python during runtime
-        plt.close(fig)
+        dir_pth = figure_path + 'plot_load_time_func/'
+        file_name = f'{dir_pth}{var}_{fname}_{aspect[0]}A{aspect[1]}_{start.strftime("%Y%m%d%H")}_{stop.strftime("%Y%m%d%H")}_{freq}F'
+        
+        self.__save_show_fig(fig, dir_pth, file_name)
     
-    def plot_load(self, var, start, stop,freq):
+    def plot_load(self, var, start, stop,freq=1,aspect=(12,5),skip_bottom_labels=False):
         """Plot/save function of load variable/s
         
         Parameters
@@ -477,7 +488,7 @@ class DataPlotter:
         """
         #assert var in self.lreader.get_vars(), f'variable "{var}" not found' # assert not needed 
         
-        fig, ax = plt.subplots(figsize=(12,5))
+        fig, ax = plt.subplots(figsize=aspect)
         
         drange = pd.date_range(start,stop,freq=f'{freq}H')
         
@@ -490,20 +501,16 @@ class DataPlotter:
         ax.set_ylim(-2000, 120000)
         ax.legend(loc='upper right')
         
-        # rotate labels from x-axis by 30° for readability
-        plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
-        
-        if self.save:
-            pth = figure_path + 'load_plot/'
-            file_name = pth + f'{"_".join([varname[:7] for varname in var])}_{start.strftime("%Y%m%d%H")}_{stop.strftime("%Y%m%d%H")}_{freq}F'
-            print(f'saving plot in {file_name}')
-            if not os.path.exists(pth):
-                os.makedirs(pth)
-            fig.savefig(f'{file_name}.{self.fmt}', bbox_inches='tight', format=self.fmt, optimize=True, dpi=150)
-        if self.show:
-            plt.show()
-        # close figures as they won't be closed automatically by python during runtime
-        plt.close(fig)
+        if not skip_bottom_labels:
+            # rotate labels from x-axis by 30° for readability
+            plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
+        else:
+            ax.set_xticklabels([])
+            
+        dir_pth = figure_path + 'load_plot/'
+        file_name = f'{dir_pth}{"_".join([varname[:7] for varname in var])}_{aspect[0]}A{aspect[1]}_'\
+                     '{start.strftime("%Y%m%d%H")}_{stop.strftime("%Y%m%d%H")}_{freq}F'
+        self.__save_show_fig(fig, dir_pth, file_name)
 
 
 fmt='pdf'
@@ -514,20 +521,22 @@ n=1
 funcs = [np.nanmin,np.nanmax,np.nanvar,np.nanmean,np.nanmedian,np.nansum]
 
 start = pd.Timestamp(2015,1,1,12)
-stop = pd.Timestamp(2018,12,31,12)
+stop = pd.Timestamp(2016,12,31,12)
+#start = pd.Timestamp(2017,1,1,12)
+#stop = pd.Timestamp(2018,12,31,12)
 freq = 24
 
-pl = DataPlotter(fmt,save=True,show=True)#,shape=(2,2))
+pl = DataPlotter(fmt,save=True,show=False,isin=True)#,shape=(2,2))
 
 rd= WeatherReader()
-pl.plot_load([de_load,hertz_load,amprion_load,tennet_load,transnet_load], start, stop, freq)
+#pl.plot_load([de_load,hertz_load,amprion_load,tennet_load,transnet_load], start, stop)
 #for var in rd.get_vars():
     #for func in funcs:
         #pl.plot_load_time_func(var,start,stop,func)
 
-#pl.plot_load_time_func(var, start, stop, np.mean)
+#pl.plot_load_time_func(var, start, stop, np.mean,aspect=(18,5),skip_bottom_labels=True)
 
-#pl.plot_nmax_var(var,4)
+pl.plot_nmax_var(var,1)
 #pl.plot_isin()
 #rd = WeatherReader()
 #for var in rd.get_vars():
